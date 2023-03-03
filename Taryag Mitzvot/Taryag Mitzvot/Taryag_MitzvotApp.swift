45db5ce3128c6a8fbac5b44a029994f668d0dd09
Snir Avrahami <sniravrahami@Snirs-MacBook-Pro.local>
2023-03-03 09:37:36 -0700

    first commit

---
+++ Taryag Mitzvot/Taryag Mitzvot/Taryag_MitzvotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taryag_MitzvotApp.swift
+//  Taryag Mitzvot
+//
+//  Created by Snir Avrahami on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Taryag_MitzvotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
